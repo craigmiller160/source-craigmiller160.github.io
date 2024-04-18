3b960473ef0b2734c3a0155146ea11e6f4d9973d
Craig Miller <craigmiller160@gmail.com>
2024-04-18 17:36:58 -0400

    Progress

--- src/resume-downloader/index.ts
+++ src/resume-downloader/index.ts
@@ -1,26 +1,40 @@
 import path from 'path';
 import { google } from 'googleapis';
+import fs from 'fs/promises';
+import { z } from 'zod';
 
-const KEYFILE_PATH = path.join(process.cwd(), 'market-tracker-service-account.json');
+const KEYFILE_PATH = path.join(
+	process.cwd(),
+	'market-tracker-service-account.json'
+);
 const SCOPES = ['https://www.googleapis.com/auth/drive'];
 const FILE_ID = '1KNIsz3VBRhLTX9wLp8NhaeVzc1z4VMIQMDzvTK2MuNA';
+const OUTPUT_PATH = path.join(
+	import.meta.dirname,
+	'..',
+	'resume',
+	'my-resume.txt'
+);
+
+const resumeResponseSchema = z.string();
 
 const auth = new google.auth.GoogleAuth({
-    keyFile: KEYFILE_PATH,
-    scopes: SCOPES
+	keyFile: KEYFILE_PATH,
+	scopes: SCOPES
 });
 
 const drive = google.drive({
-    version: 'v3',
-    auth
+	version: 'v3',
+	auth
 });
 
 const downloadResume = async () => {
-    const response = await drive.files.export({
-        fileId: FILE_ID,
-        mimeType: 'text/plain'
-    });
-    console.log(response);
+	const response = await drive.files.export({
+		fileId: FILE_ID,
+		mimeType: 'text/plain'
+	});
+	const resumeText = resumeResponseSchema.parse(response.data);
+	await fs.writeFile(OUTPUT_PATH, resumeText);
 };
 
 downloadResume();
