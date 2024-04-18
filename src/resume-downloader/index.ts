eb43d40358b7e4edd7afec92e13c8a1db5126510
Craig Miller <craigmiller160@gmail.com>
2024-04-18 17:32:49 -0400

    Progress

--- src/resume-downloader/index.ts
+++ src/resume-downloader/index.ts
@@ -3,6 +3,7 @@ import { google } from 'googleapis';
 
 const KEYFILE_PATH = path.join(process.cwd(), 'market-tracker-service-account.json');
 const SCOPES = ['https://www.googleapis.com/auth/drive'];
+const FILE_ID = '1KNIsz3VBRhLTX9wLp8NhaeVzc1z4VMIQMDzvTK2MuNA';
 
 const auth = new google.auth.GoogleAuth({
     keyFile: KEYFILE_PATH,
@@ -14,3 +15,12 @@ const drive = google.drive({
     auth
 });
 
+const downloadResume = async () => {
+    const response = await drive.files.export({
+        fileId: FILE_ID,
+        mimeType: 'text/plain'
+    });
+    console.log(response);
+};
+
+downloadResume();
