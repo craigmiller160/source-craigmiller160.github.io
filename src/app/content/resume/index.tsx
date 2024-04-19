e8bbc1a3274f8d018efa560295a7349d48f259b0
Craig Miller <craigmiller160@gmail.com>
2024-04-19 16:06:18 -0400

    Refactoring use of resume

--- src/app/content/resume/index.tsx
+++ src/app/content/resume/index.tsx
@@ -1,42 +1,38 @@
-import { type Resume, resumeSchema } from '../../../resume/resume';
+import { resumeSchema } from '../../../resume/resume';
 import myResume from '../../../resume/my-resume.json';
-import { useRef } from 'react';
 import { Typography } from 'antd';
 import classes from './Result.module.scss';
 
-export const ResumePage = () => {
-	const resume = useRef<Resume>(resumeSchema.parse(myResume));
-	return (
-		<div className={classes.resume}>
-			<section className={classes.section}>
-				<div className={classes.row}>
-					<Typography.Title level={3}>
-						{resume.current.name}
-					</Typography.Title>
-					<Typography.Title level={3}>
-						{resume.current.contact.email}
-					</Typography.Title>
-				</div>
-			</section>
-			<section className={classes.section}>
-				<div className={classes.row}>
-					<Typography.Title level={3}>
-						{resume.current.intro.title}
-					</Typography.Title>
-				</div>
-				<div className={classes.row}>
-					<Typography.Text>
-						{resume.current.intro.body}
-					</Typography.Text>
-				</div>
-			</section>
-			<section className={classes.section}>
-				<div className={classes.row}>
-					<Typography.Title level={3}>
-						Industry Experience
-					</Typography.Title>
-				</div>
-			</section>
-		</div>
-	);
-};
+const resume = resumeSchema.parse(myResume);
+
+export const ResumePage = () => (
+	<div className={classes.resume}>
+		<section className={classes.section}>
+			<div className={classes.row}>
+				<Typography.Title level={3}>
+					{resume.current.name}
+				</Typography.Title>
+				<Typography.Title level={3}>
+					{resume.current.contact.email}
+				</Typography.Title>
+			</div>
+		</section>
+		<section className={classes.section}>
+			<div className={classes.row}>
+				<Typography.Title level={3}>
+					{resume.current.intro.title}
+				</Typography.Title>
+			</div>
+			<div className={classes.row}>
+				<Typography.Text>{resume.current.intro.body}</Typography.Text>
+			</div>
+		</section>
+		<section className={classes.section}>
+			<div className={classes.row}>
+				<Typography.Title level={3}>
+					Industry Experience
+				</Typography.Title>
+			</div>
+		</section>
+	</div>
+);
