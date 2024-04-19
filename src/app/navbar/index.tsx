8f697f053f478abcd03b808b561d3a612604cfd6
Craig Miller <craigmiller160@gmail.com>
2024-04-19 13:55:20 -0400

    Updating menu

--- src/app/navbar/index.tsx
+++ src/app/navbar/index.tsx
@@ -1,21 +1,36 @@
 import { Layout, Menu, type MenuProps } from 'antd';
+import { type MenuInfo } from 'rc-menu/lib/interface';
 import classes from './Navbar.module.scss';
 import classNames from 'classnames';
+import { useState } from 'react';
+
+const NOTHING_KEY = 'nothing';
+const RESUME_KEY = 'resume';
+type MenuKey = typeof NOTHING_KEY | typeof RESUME_KEY;
 
 const items: MenuProps['items'] = [
 	{
-		key: 'nothing',
+		key: NOTHING_KEY,
 		className: classNames(classes.brand, classes.item),
 		label: 'Craig Miller'
 	},
 	{
-		key: 'resume',
+		key: RESUME_KEY,
 		label: 'Resume',
 		className: classes.item
 	}
 ];
 
 export const Navbar = () => {
+	const [menuKey, setMenuKey] = useState<MenuKey>('resume');
+
+	const onMenuClick = (info: MenuInfo) => {
+		if (info.key === NOTHING_KEY) {
+			return;
+		}
+		setMenuKey(info.key as MenuKey);
+	};
+
 	return (
 		<Layout>
 			<Layout.Header className={classes.navbar}>
@@ -24,6 +39,8 @@ export const Navbar = () => {
 					theme="light"
 					mode="horizontal"
 					items={items}
+					selectedKeys={[menuKey]}
+					onClick={onMenuClick}
 				/>
 			</Layout.Header>
 		</Layout>
