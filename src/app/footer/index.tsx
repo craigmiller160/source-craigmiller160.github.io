5a112f3d202bfc6570954f8d7498aa4911e46d0e
Craig Miller <craigmiller160@gmail.com>
2024-04-20 13:00:29 -0400

    Added footer content

--- src/app/footer/index.tsx
+++ src/app/footer/index.tsx
@@ -1,7 +1,16 @@
-import { Layout } from 'antd';
+import { Layout, Typography } from 'antd';
+import classes from './Footer.module.scss';
 
 export const Footer = () => (
-	<Layout.Footer>
-		<p>Hello</p>
+	<Layout.Footer className={classes.footer}>
+		<div className={classes.footerContent}>
+			<Typography.Title level={5}>Attributions</Typography.Title>
+			<a
+				href="https://www.flaticon.com/free-icons/program"
+				title="program icons"
+			>
+				Program icons created by Bharat Icons - Flaticon
+			</a>
+		</div>
 	</Layout.Footer>
 );
