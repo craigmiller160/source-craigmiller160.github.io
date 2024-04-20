25e1e3f02623062664da397d04ba001dcdfdac72
Craig Miller <craigmiller160@gmail.com>
2024-04-20 11:16:29 -0400

    Updated styling of skills

--- src/app/content/resume/Skills.module.scss.d.ts
+++ src/app/content/resume/Skills.module.scss.d.ts
@@ -1,3 +1,4 @@
+export declare const category: string;
 export declare const col: string;
 export declare const row: string;
 export declare const skills: string;
--- src/app/content/resume/Skills.tsx
+++ src/app/content/resume/Skills.tsx
@@ -1,5 +1,6 @@
 import type { Resume } from '../../../resume/resume';
 import classes from './Skills.module.scss';
+import classNames from 'classnames';
 import { Typography } from 'antd';
 
 type Props = Readonly<{
@@ -12,19 +13,20 @@ export const Skills = (props: Props) => (
 			<Typography.Title level={3}>Technical Knowledge</Typography.Title>
 		</div>
 		<div className={classes.row}>
-			<div className={classes.col}>
+			<div className={classNames(classes.col, classes.category)}>
 				<Typography.Title level={5}>Languages</Typography.Title>
+				{props.resume.skills.languages.join(', ')}
 			</div>
-			<div className={classes.col}>
+			<div className={classNames(classes.col, classes.category)}>
 				<Typography.Title level={5}>Frameworks/Tools</Typography.Title>
 			</div>
-			<div className={classes.col}>
+			<div className={classNames(classes.col, classes.category)}>
 				<Typography.Title level={5}>Databases</Typography.Title>
 			</div>
-			<div className={classes.col}>
+			<div className={classNames(classes.col, classes.category)}>
 				<Typography.Title level={5}>Cloud Deployment</Typography.Title>
 			</div>
-			<div className={classes.col}>
+			<div className={classNames(classes.col, classes.category)}>
 				<Typography.Title level={5}>Agile Experience</Typography.Title>
 			</div>
 		</div>
