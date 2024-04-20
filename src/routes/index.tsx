ea3a68f2a7f2dd445a81ba6b96ac0b4a6eebefbc
Craig Miller <craigmiller160@gmail.com>
2024-04-20 18:24:00 -0400

    Stubbed out Expense Tracker page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,15 @@
 import { Navigate, type RouteObject } from 'react-router';
 import { ResumePage } from '../app/content/resume';
+import { ExpenseTracker } from '../app/content/projects/expense-tracker';
 
 export const routes: RouteObject[] = [
 	{
 		path: '/resume',
 		element: <ResumePage />
+	},
+	{
+		path: '/projects/expense-tracker',
+		element: <ExpenseTracker />
 	},
 	{
 		path: '*',
