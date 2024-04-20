698f6f9ec77afe915d2a1ac386bfa0a98d1d508f
Craig Miller <craigmiller160@gmail.com>
2024-04-20 10:50:47 -0400

    More styling fixes

--- src/app/content/resume/index.tsx
+++ src/app/content/resume/index.tsx
@@ -1,6 +1,6 @@
 import { type Job, resumeSchema } from '../../../resume/resume';
 import myResume from '../../../resume/my-resume.json';
-import { Typography } from 'antd';
+import { Card, Typography } from 'antd';
 import classes from './Resume.module.scss';
 import classNames from 'classnames';
 
@@ -25,33 +25,38 @@ const Experience = (props: ExperienceProps) => (
 
 export const ResumePage = () => (
 	<div className={classes.resume}>
-		<section className={classes.section}>
-			<div className={classes.row}>
-				<Typography.Title level={3}>{resume.name}</Typography.Title>
-				<Typography.Title level={3} className={classes.marginTopFix}>
-					{resume.contact.email}
-				</Typography.Title>
-			</div>
-		</section>
-		<section className={classes.section}>
-			<div className={classes.row}>
-				<Typography.Title level={3}>
-					{resume.intro.title}
-				</Typography.Title>
-			</div>
-			<div className={classes.row}>
-				<Typography.Text>{resume.intro.body}</Typography.Text>
-			</div>
-		</section>
-		<section className={classes.section}>
-			<div className={classes.row}>
-				<Typography.Title level={3}>
-					Industry Experience
-				</Typography.Title>
-			</div>
-			{resume.experience.map((job, index) => (
-				<Experience key={index} job={job} />
-			))}
-		</section>
+		<Card>
+			<section className={classes.section}>
+				<div className={classes.row}>
+					<Typography.Title level={3}>{resume.name}</Typography.Title>
+					<Typography.Title
+						level={3}
+						className={classes.marginTopFix}
+					>
+						{resume.contact.email}
+					</Typography.Title>
+				</div>
+			</section>
+			<section className={classes.section}>
+				<div className={classes.row}>
+					<Typography.Title level={3}>
+						{resume.intro.title}
+					</Typography.Title>
+				</div>
+				<div className={classes.row}>
+					<Typography.Text>{resume.intro.body}</Typography.Text>
+				</div>
+			</section>
+			<section className={classes.section}>
+				<div className={classes.row}>
+					<Typography.Title level={3}>
+						Industry Experience
+					</Typography.Title>
+				</div>
+				{resume.experience.map((job, index) => (
+					<Experience key={index} job={job} />
+				))}
+			</section>
+		</Card>
 	</div>
 );
