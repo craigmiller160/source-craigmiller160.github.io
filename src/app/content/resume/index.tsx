6a25fc805e920bee9b03b6b5ec944edc11e6bb66
Craig Miller <craigmiller160@gmail.com>
2024-04-20 11:01:30 -0400

    More refactoring

--- src/app/content/resume/index.tsx
+++ src/app/content/resume/index.tsx
@@ -1,35 +1,16 @@
 import { resumeSchema } from '../../../resume/resume';
 import myResume from '../../../resume/my-resume.json';
-import { Card, Typography } from 'antd';
+import { Card } from 'antd';
 import classes from './Resume.module.scss';
 import { Experience } from './Experience';
+import { Introduction } from './Introduction';
 
 const resume = resumeSchema.parse(myResume);
 
 export const ResumePage = () => (
 	<div className={classes.resume}>
 		<Card>
-			<section className={classes.section}>
-				<div className={classes.row}>
-					<Typography.Title level={3}>{resume.name}</Typography.Title>
-					<Typography.Title
-						level={3}
-						className={classes.marginTopFix}
-					>
-						{resume.contact.email}
-					</Typography.Title>
-				</div>
-			</section>
-			<section className={classes.section}>
-				<div className={classes.row}>
-					<Typography.Title level={3}>
-						{resume.intro.title}
-					</Typography.Title>
-				</div>
-				<div className={classes.row}>
-					<Typography.Text>{resume.intro.body}</Typography.Text>
-				</div>
-			</section>
+			<Introduction resume={resume} />
 			<Experience resume={resume} />
 		</Card>
 	</div>
