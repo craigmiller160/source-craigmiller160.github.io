2b07c1cf24bc52e5e253a6df519b5ce5f17578c2
Craig Miller <craigmiller160@gmail.com>
2024-04-21 09:53:56 -0400

    Working on new page

---
+++ src/app/content/projects/expense-tracker/ExpenseTracker.module.scss.d.ts
@@ -0,0 +1,4 @@
+export declare const col: string;
+export declare const expenseTracker: string;
+export declare const pageTitle: string;
+export declare const row: string;
--- src/app/content/projects/expense-tracker/index.tsx
+++ src/app/content/projects/expense-tracker/index.tsx
@@ -1,7 +1,13 @@
+import classes from './ExpenseTracker.module.scss';
+import classNames from 'classnames';
+import {Typography} from 'antd';
+
 export const ExpenseTracker = () => {
 	return (
-		<div>
-			<h1>Expense Tracker</h1>
+		<div className={classes.expenseTracker}>
+			<div className={classNames(classes.row, classes.pageTitle)}>
+				<Typography.Title level={3}>Expense Tracker Project</Typography.Title>
+			</div>
 		</div>
 	);
 };
