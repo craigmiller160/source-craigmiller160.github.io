0e4ef1984b6c0dbf3709bd77eb95b6c254f0310c
Craig Miller <craigmiller160@gmail.com>
2024-04-21 10:18:08 -0400

    Making the projects page controlled by dev flag

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,17 +2,25 @@ import { Navigate, type RouteObject } from 'react-router';
 import { ResumePage } from '../app/content/resume';
 import { ExpenseTracker } from '../app/content/projects/expense-tracker';
 
-export const routes: RouteObject[] = [
+const allRoutes: RouteObject[] = [
 	{
 		path: '/resume',
 		element: <ResumePage />
 	},
 	{
-		path: '/projects/expense-tracker',
-		element: <ExpenseTracker />
-	},
-	{
 		path: '*',
 		element: <Navigate to="/resume" />
 	}
+];
+
+const devRoutes: RouteObject[] = [
+	{
+		path: '/projects/expense-tracker',
+		element: <ExpenseTracker />
+	}
+];
+
+export const routes: RouteObject[] = [
+	...allRoutes,
+	...(import.meta.env.DEV ? devRoutes : [])
 ];
