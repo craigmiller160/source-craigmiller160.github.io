e4bd4b5493d799f6ab198f75e7fb6f6d2b6a3042
Craig Miller <craigmiller160@gmail.com>
2024-04-22 17:30:54 -0400

    Added craig-build page

---
+++ src/app/content/projects/craig-build/CraigBuild.module.scss.d.ts
@@ -0,0 +1,4 @@
+export declare const col: string;
+export declare const craigBuild: string;
+export declare const header: string;
+export declare const row: string;
---
+++ src/app/content/projects/craig-build/index.tsx
@@ -0,0 +1,44 @@
+import classes from './CraigBuild.module.scss';
+import { Card, Typography } from 'antd';
+import classNames from 'classnames';
+
+export const CraigBuild = () => (
+	<div className={classes.craigBuild}>
+		<div className={classNames(classes.row, classes.header)}>
+			<Typography.Title level={3}>
+				Project Build System (aka &apos;craig-build&apos;)
+			</Typography.Title>
+		</div>
+		<Card className={classes.col}>
+			<div className={classNames(classes.row, classes.header)}>
+				<Typography.Title level={5}>Introduction</Typography.Title>
+			</div>
+			<div className={classes.col}>
+				<Typography.Text>
+					Over the years, I&apos;ve built a lot of personal projects,
+					far more even than I&apos;m showcasing in this portfolio. My
+					years in the industry demonstrated the value of
+					standardized, uniform build tools. While I have yet to build
+					a full CI/CD for myself, this tool is pretty close.
+				</Typography.Text>
+				<Typography.Text>
+					The only thing separating this tool, which I call
+					&apos;craig-build&apos;, from a real CI/CD is the fact that
+					it is not automated. I need to invoke it on my machine via a
+					command. However, with a single command it will scan the
+					project, identify which type of project it is, validate the
+					configuration, build, test, and deploy it. It is the heart
+					of my current home infrastructure, and one day maybe I'll
+					turn it into a proper CI/CD.
+				</Typography.Text>
+			</div>
+		</Card>
+		<Card>
+			<div className={classNames(classes.row, classes.header)}>
+				<Typography.Title level={5}>
+					More Details Coming Soon...
+				</Typography.Title>
+			</div>
+		</Card>
+	</div>
+);
