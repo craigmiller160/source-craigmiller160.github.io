de0febeae8f2ee415f620c06cca3aeb87e722f7d
Craig Miller <craigmiller160@gmail.com>
2024-04-21 20:06:35 -0400

    Figuring out image system

--- src/app/content/about-me/index.tsx
+++ src/app/content/about-me/index.tsx
@@ -1,7 +1,13 @@
 import classes from './AboutMe.module.scss';
+import { Card, Image } from 'antd';
+import meAndDogs from '../../../images/me_and_dogs.jpeg';
 
 export const AboutMe = () => (
 	<div className={classes.aboutMe}>
-		<h1>About Me</h1>
+		<Card>
+			<div>
+				<Image src={meAndDogs} width="40%" />
+			</div>
+		</Card>
 	</div>
 );
