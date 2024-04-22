eca6dcac05fa80153b9373426391edcc680322ba
Craig Miller <craigmiller160@gmail.com>
2024-04-21 20:30:20 -0400

    Adjusted layout

--- src/app/content/about-me/AboutMe.module.scss.d.ts
+++ src/app/content/about-me/AboutMe.module.scss.d.ts
@@ -1,5 +1,6 @@
 export declare const aboutMe: string;
 export declare const col: string;
+export declare const contentWrapper: string;
 export declare const imageWrapper: string;
 export declare const marginTopFix: string;
 export declare const row: string;
--- src/app/content/about-me/index.tsx
+++ src/app/content/about-me/index.tsx
@@ -1,21 +1,26 @@
 import classes from './AboutMe.module.scss';
 import { Card, Image, Typography } from 'antd';
 import meAndDogs from '../../../images/me_and_dogs.jpeg';
+import classNames from 'classnames';
 
 export const AboutMe = () => (
 	<div className={classes.aboutMe}>
 		<Card>
 			<section className={classes.row}>
 				<Typography.Title level={3}>Craig Miller</Typography.Title>
-				<Typography.Title level={3} className={classes.marginTopFix}>(He/Him)</Typography.Title>
+				<Typography.Title level={3} className={classes.marginTopFix}>
+					(He/Him)
+				</Typography.Title>
 			</section>
-			<section className={classes.imageWrapper}>
-				<Image src={meAndDogs} width="40%" />
-			</section>
-			<section className={classes.col}>
-				<Typography.Text>
-					Welcome to the personal portfolio of Craig Miller.{' '}
-				</Typography.Text>
+			<section
+				className={classNames(classes.row, classes.contentWrapper)}
+			>
+				<Image src={meAndDogs} />
+				<div className={classes.col}>
+					<Typography.Text>
+						Welcome to the personal portfolio of Craig Miller.{' '}
+					</Typography.Text>
+				</div>
 			</section>
 		</Card>
 	</div>
