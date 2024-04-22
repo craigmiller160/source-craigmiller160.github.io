b5e30662ddf9da8ba7ed309a8053424171c22f46
Craig Miller <craigmiller160@gmail.com>
2024-04-22 15:10:06 -0400

    Updated resume email size

--- src/app/content/resume/Introduction.tsx
+++ src/app/content/resume/Introduction.tsx
@@ -1,32 +1,39 @@
 import type { Resume } from '../../../resume/resume';
 import classes from './Introduction.module.scss';
 import { Typography } from 'antd';
+import { useSubTitleSize } from '../../../ui/responsive/useSubTitleSize';
 
 type Props = Readonly<{
 	resume: Resume;
 }>;
 
-export const Introduction = (props: Props) => (
-	<div>
-		<section>
-			<div className={classes.row}>
-				<Typography.Title level={3}>
-					{props.resume.name}
-				</Typography.Title>
-				<Typography.Title level={3} className={classes.marginTopFix}>
-					{props.resume.contact.email}
-				</Typography.Title>
-			</div>
-		</section>
-		<section>
-			<div className={classes.row}>
-				<Typography.Title level={3}>
-					{props.resume.intro.title}
-				</Typography.Title>
-			</div>
-			<div className={classes.row}>
-				<Typography.Text>{props.resume.intro.body}</Typography.Text>
-			</div>
-		</section>
-	</div>
-);
+export const Introduction = (props: Props) => {
+	const subTitleSize = useSubTitleSize();
+	return (
+		<div>
+			<section>
+				<div className={classes.row}>
+					<Typography.Title level={3}>
+						{props.resume.name}
+					</Typography.Title>
+					<Typography.Title
+						level={subTitleSize}
+						className={classes.marginTopFix}
+					>
+						{props.resume.contact.email}
+					</Typography.Title>
+				</div>
+			</section>
+			<section>
+				<div className={classes.row}>
+					<Typography.Title level={3}>
+						{props.resume.intro.title}
+					</Typography.Title>
+				</div>
+				<div className={classes.row}>
+					<Typography.Text>{props.resume.intro.body}</Typography.Text>
+				</div>
+			</section>
+		</div>
+	);
+};
