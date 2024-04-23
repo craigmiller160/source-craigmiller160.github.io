8ef72fa7e5a698572c480225321ef7ac1f66bdbf
Craig Miller <craigmiller160@gmail.com>
2024-04-22 22:47:22 -0400

    More work on oauth2 server

--- src/app/content/projects/oauth2-server/index.tsx
+++ src/app/content/projects/oauth2-server/index.tsx
@@ -1,3 +1,29 @@
-export const Oauth2Server = () => {
+import classes from './OAuth2Server.module.scss';
+import classNames from 'classnames';
+import { Card, Typography } from 'antd';
 
-}
+export const Oauth2Server = () => (
+	<div className={classes.oauth2Server}>
+		<div className={classNames(classes.row, classes.header)}>
+			<Typography.Title level={3}>
+				OAuth2 Server Project (Retired)
+			</Typography.Title>
+		</div>
+		<Card className={classes.col}>
+			<div className={classNames(classes.row, classes.header)}>
+				<Typography.Title level={5}>Introduction</Typography.Title>
+			</div>
+			<div className={classes.col}>
+				<Typography.Text></Typography.Text>
+				<Typography.Text></Typography.Text>
+			</div>
+		</Card>
+		<Card>
+			<div className={classNames(classes.row, classes.header)}>
+				<Typography.Title level={5}>
+					More Details Coming Soon...
+				</Typography.Title>
+			</div>
+		</Card>
+	</div>
+);
