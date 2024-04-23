bcdc856a1b06a5adfb0d50876eb83185b6f53d3b
Craig Miller <craigmiller160@gmail.com>
2024-04-23 17:46:37 -0400

    Working on navbar

--- src/app/navbar_v2/index.tsx
+++ src/app/navbar_v2/index.tsx
@@ -1,10 +1,18 @@
 import classes from './Navbar.module.scss';
 import { NavLink } from 'react-router-dom';
+import { items, type NavbarItem } from './items';
+import { type ReactNode, useMemo } from 'react';
 
-export const Navbar = () => (
-	<header className={classes.navbar}>
-		<div className={classes.menu}>
-			<h3>Hello World</h3>
-		</div>
-	</header>
-);
+const navbarItemToDesktopComponent = (item: NavbarItem): ReactNode => {};
+
+const useDesktopItems = () =>
+	useMemo(() => items.map(navbarItemToDesktopComponent), []);
+
+export const Navbar = () => {
+	const desktopItems = useDesktopItems();
+	return (
+		<header className={classes.navbar}>
+			<div className={classes.menu}>{desktopItems}</div>
+		</header>
+	);
+};
