bcdfb1c2e3114db08b270010a527658505f57b05
Craig Miller <craigmiller160@gmail.com>
2024-04-22 22:50:28 -0400

    Updated oauth2 server page

--- src/app/content/projects/oauth2-server/index.tsx
+++ src/app/content/projects/oauth2-server/index.tsx
@@ -14,8 +14,22 @@ export const Oauth2Server = () => (
 				<Typography.Title level={5}>Introduction</Typography.Title>
 			</div>
 			<div className={classes.col}>
-				<Typography.Text></Typography.Text>
-				<Typography.Text></Typography.Text>
+				<Typography.Text>
+					One of the most common parts of an application is its
+					authentication & authorization solution. During the
+					pandemic, I had quite a bit of time on my hands, so I
+					decided to write my own OAuth2 authorization server from
+					scratch. I read the entire RFC specification and followed it
+					to the letter, implementing a fully compliant OAuth2
+					solution supporting multiple grant types, including the
+					ever-important authorization code flow.
+				</Typography.Text>
+				<Typography.Text>
+					Ultimately I retired this project in favor of moving to an
+					off-the-shelf solution (Keycloak) for my ecosystem. However,
+					it was a really enjoyable project for the time that I had
+					it.
+				</Typography.Text>
 			</div>
 		</Card>
 		<Card>
