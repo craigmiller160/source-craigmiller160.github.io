e29c1a060d02a8b3a4c35df43307dedf176a7c0a
Craig Miller <craigmiller160@gmail.com>
2024-04-25 08:15:04 -0400

    Updating items

--- src/app/content/projects/Projects.module.scss.d.ts
+++ src/app/content/projects/Projects.module.scss.d.ts
@@ -1,3 +1,4 @@
 export declare const col: string;
 export declare const projects: string;
 export declare const row: string;
+export declare const title: string;
--- src/app/content/projects/index.tsx
+++ src/app/content/projects/index.tsx
@@ -1,11 +1,46 @@
 import classes from './Projects.module.scss';
-import { Typography } from 'antd';
+import { List, Typography } from 'antd';
+import classNames from 'classnames';
+import type { ReactNode } from 'react';
+import { type NavigateFunction, useNavigate } from 'react-router';
+import { DollarOutlined } from '@ant-design/icons';
+import { navbar } from '../../navbar/Navbar.module.scss';
+
+type ListDataItem = Readonly<{
+	label: string;
+	icon: ReactNode;
+	onClick: () => void;
+}>;
+
+const createListItems = (navigate: NavigateFunction): ListDataItem[] => [
+	{
+		label: 'Expense Tracker',
+		icon: <DollarOutlined />,
+		onClick: () => navigate('/projects/expense-tracker')
+	}
+];
 
 export const Projects = () => {
+	const navigate = useNavigate();
+	const items = createListItems(navigate);
+
 	return (
 		<div className={classes.projects}>
+			<div className={classNames(classes.row, classes.title)}>
+				<Typography.Title level={3}>Personal Projects</Typography.Title>
+			</div>
 			<div className={classes.row}>
-				<Typography.Title level={3}>Personal Projects 2</Typography.Title>
+				<List
+					dataSource={items}
+					renderItem={(item) => (
+						<List.Item>
+							<List.Item.Meta
+								avatar={item.icon}
+								title={item.label}
+							/>
+						</List.Item>
+					)}
+				/>
 			</div>
 		</div>
 	);
