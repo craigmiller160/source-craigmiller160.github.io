b92f52ce093449ae8de19da3de161cd4f68e3fc2
Craig Miller <craigmiller160@gmail.com>
2024-04-27 16:22:44 -0400

    Updated resume code

--- src/app/content/resume/Experience.tsx
+++ src/app/content/resume/Experience.tsx
@@ -12,19 +12,22 @@ type JobProps = Readonly<{
 	job: Job;
 }>;
 
-const Job = (props: JobProps) => (
-	<div className={classNames(classes.col, classes.job)}>
-		<Typography.Title level={5} className={classes.title}>
-			{props.job.company} ({props.job.dates})
-		</Typography.Title>
-		<Typography.Text className={classes.positions}>
-			{props.job.positions
-				.map((position) => `${position.title} (${position.dates})`)
-				.join(', ')}
-		</Typography.Text>
-		<BulletList items={props.job.achievements} />
-	</div>
-);
+const Job = (props: JobProps) => {
+	const dates = props.job.dates.length > 0 ? `(${props.job.dates})` : '';
+	return (
+		<div className={classNames(classes.col, classes.job)}>
+			<Typography.Title level={5} className={classes.title}>
+				{props.job.company} {dates}
+			</Typography.Title>
+			<Typography.Text className={classes.positions}>
+				{props.job.positions
+					.map((position) => `${position.title} (${position.dates})`)
+					.join(', ')}
+			</Typography.Text>
+			<BulletList items={props.job.achievements} />
+		</div>
+	);
+};
 
 export const Experience = (props: ExperienceProps) => (
 	<section className={classes.experience}>
