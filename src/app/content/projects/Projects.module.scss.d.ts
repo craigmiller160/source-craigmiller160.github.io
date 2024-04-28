6d62f626c07a4de453f2793dd0d648686cbb3b0f
Craig Miller <craigmiller160@gmail.com>
2024-04-28 13:21:13 -0400

    Creating new LinkList component

--- src/app/content/projects/Projects.module.scss.d.ts
+++ src/app/content/projects/Projects.module.scss.d.ts
@@ -1,6 +1,5 @@
 export declare const col: string;
 export declare const list: string;
-export declare const listItem: string;
 export declare const listWrapper: string;
 export declare const projects: string;
 export declare const row: string;
---
+++ src/ui/LinkList/index.tsx
@@ -0,0 +1,50 @@
+import type { ReactNode } from 'react';
+import { useNavigate } from 'react-router';
+import { List } from 'antd';
+import classNames from 'classnames';
+import classes from './LinkList.module.scss';
+import { castDraft } from 'immer';
+
+type Item = Readonly<{
+	label: string;
+	link: string;
+	icon?: ReactNode;
+}>;
+
+type Props = Readonly<{
+	listClassName?: string;
+	listItemClassName?: string;
+	items: ReadonlyArray<Item>;
+}>;
+
+const useExtendedNavigate = (): ((p: string) => void) => {
+	const navigate = useNavigate();
+	return (path) => {
+		if (/^https?/.test(path)) {
+			window.open(path, '_blank');
+			return;
+		}
+		navigate(path);
+	};
+};
+
+export const LinkList = (props: Props) => {
+	const navigate = useExtendedNavigate();
+	return (
+		<List
+			className={classNames(classes.linkList, props.listClassName)}
+			dataSource={castDraft(props.items)}
+			renderItem={(item) => (
+				<List.Item
+					className={classNames(
+						classes.listItem,
+						props.listItemClassName
+					)}
+					onClick={() => navigate(item.link)}
+				>
+					<List.Item.Meta avatar={item.icon} title={item.label} />
+				</List.Item>
+			)}
+		/>
+	);
+};
