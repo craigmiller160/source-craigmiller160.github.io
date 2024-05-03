f951110772dfd11369d34416769561074cc5af68
Craig Miller <craigmiller160@gmail.com>
2024-05-02 20:00:21 -0400

    Working on pages

--- src/app/content/projects/market-tracker/marketTrackerDocs.ts
+++ src/app/content/projects/market-tracker/marketTrackerDocs.ts
@@ -1,4 +1,5 @@
 import type { Documentation } from '../documentation-common/Documentation';
+import marketTrackerRecognition from '../../../../images/marketTrackerRecognition.png';
 
 export const marketTrackerDocs: ReadonlyArray<Documentation> = [
 	{
@@ -7,6 +8,19 @@ export const marketTrackerDocs: ReadonlyArray<Documentation> = [
 		text: [
 			'I am a longtime, loyal customer of a traditional investment bank. I find they offer all the services I need, and when I have an issue their customer service is exceptional. However, their technology offerings are definitely sub-par. To better observe market movements, both with my own investments and others that I just want to watch, I wrote this application.',
 			'This app leverages free APIs to pull real-time market data to drive its functionality. It allows for both defining investment watchlists, while also having deep integration with my own investment record keeping. Through Google Drive connectivity, every time I update the spreadsheets I use to track my investments, the data is reflected in this application, displaying how market movements are reflecting my actual investment holdings.'
+		]
+	},
+	{
+		type: 'image-only',
+		title: 'Architecture Diagram',
+		image: 'TBD'
+	},
+	{
+		type: 'image-and-text',
+		title: 'Data Sources',
+		image: marketTrackerRecognition,
+		text: [
+			'The application depends on data provided by the free APIs of Tradier and CoinGecko. Every feature of this application depends on the data from them, and a recognition page exists to grant credit for what they provide.'
 		]
 	}
 ];
