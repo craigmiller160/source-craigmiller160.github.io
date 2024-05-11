a6427a3a649bef59a45e81e1c68a1e893c48aa68
Craig Miller <craigmiller160@gmail.com>
2024-05-11 08:55:25 -0400

    Refactored more documentation

--- src/app/content/projects/oauth2-server/OAuth2Server.module.scss.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const col: string;
-export declare const oauth2Server: string;
-export declare const row: string;
-export declare const rowCenter: string;
--- src/app/content/projects/oauth2-server/index.tsx
+++ src/app/content/projects/oauth2-server/index.tsx
@@ -1,61 +1,14 @@
-import classes from './OAuth2Server.module.scss';
-import classNames from 'classnames';
-import { Card, Typography } from 'antd';
+import { fullDocs, placeholderDocs } from './oauth2ServerDocs';
+import { DocumentationPage } from '../documentation-common/DocumentationPage';
 
-export const Oauth2Server = () => (
-	<div className={classes.oauth2Server}>
-		<div className={classNames(classes.row, classes.rowCenter)}>
-			<Typography.Title level={3}>
-				OAuth2 Server Project (Retired)
-			</Typography.Title>
-		</div>
-		<Card className={classes.col}>
-			<div className={classNames(classes.row, classes.rowCenter)}>
-				<Typography.Title level={5}>Introduction</Typography.Title>
-			</div>
-			<div className={classes.col}>
-				<Typography.Text>
-					One of the most common parts of an application is its
-					authentication & authorization solution. During the
-					pandemic, I had quite a bit of time on my hands, so I
-					decided to write my own OAuth2 authorization server from
-					scratch. I read the entire RFC specification and followed it
-					to the letter, implementing a fully compliant OAuth2
-					solution supporting multiple grant types, including the
-					ever-important authorization code flow.
-				</Typography.Text>
-				<Typography.Text>
-					Ultimately I retired this project in favor of moving to an
-					off-the-shelf solution (Keycloak) for my ecosystem. However,
-					it was a really enjoyable project for the time that I had
-					it.
-				</Typography.Text>
-			</div>
-		</Card>
-		{import.meta.env.VITE_ENABLE_OAUTH2_SERVER !== 'true' && (
-			<Card>
-				<div className={classNames(classes.row, classes.rowCenter)}>
-					<Typography.Title level={5}>
-						More Details Coming Soon...
-					</Typography.Title>
-				</div>
-			</Card>
-		)}
-		{import.meta.env.VITE_ENABLE_OAUTH2_SERVER === 'true' && (
-			<Card>
-				<ul>
-					<li>Introduction</li>
-					<li>Diagram</li>
-					<li>
-						Features
-						<ul>
-							<li>Password Flow</li>
-							<li>Auth Code Flow</li>
-							<li>User Access Management UI</li>
-						</ul>
-					</li>
-				</ul>
-			</Card>
-		)}
-	</div>
-);
+export const Oauth2Server = () => {
+	const docs = import.meta.env.VITE_ENABLE_OAUTH2_SERVER
+		? fullDocs
+		: placeholderDocs;
+	return (
+		<DocumentationPage
+			title="OAuth2 Server (Retired)"
+			documentation={docs}
+		/>
+	);
+};
